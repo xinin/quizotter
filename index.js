346c63ac417e9251606c4b3cac75ab5df1e7a979
Diego Prieto <diego.prieto@keepler.io>
2019-10-05 18:16:03 +0200

    donete

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const cluesUL = document.querySelector('#clues');
 let countdown;
 let countdownFail;
 
-let done = false;
-
 function displayTimeLeft(seconds) {
   daysElement.textContent = `${Math.floor(seconds / 86400)}:`;
   hoursElement.textContent = `${Math.floor((seconds % 86400) / 3600)}:`;
@@ -17,20 +15,17 @@ function displayTimeLeft(seconds) {
 }
 
 function displayClues(seconds) {
-  if (!done) {
-    const daysLeft = 27 - Math.floor(seconds / 86400);
-    const cluesString = [
-      '<li>¿Que tienen en común estas <a target="_blank" href="./img/collage-0.png">ciudades<a/>?</li>',
-      '<li>¿Y si ademas añades estas <a target="_blank" href="./img/collage-1.png">ciudades<a/>?</li>',
-      '<li>Las ciudades son: <ul><li> Nueva York</li><li> Londres</li><li> Toronto</li><li> Melbourne </li><li> Hamburgo</li></ul>',
-      '<li>No me dió tiempo a hacerlo en Nuevo York</li>',
-      '<li>La duración es similar a un año escolar</li>',
-      '<li></li>',
-      '<li></li>',
-    ];
-    cluesUL.innerHTML = cluesString.slice(0, daysLeft + 1).toString().replace(/,/g, '');
-    done = true;
-  }
+  const daysLeft = 7 - Math.floor(seconds / 86400);
+  const cluesString = [
+    '<li>¿Que tienen en común estas <a target="_blank" href="./img/collage-0.png">ciudades<a/>?</li>',
+    '<li>¿Y si ademas añades estas <a target="_blank" href="./img/collage-1.png">ciudades<a/>?</li>',
+    '<li>Las ciudades son: <ul><li>Nueva York</li><li>Londres</li><li>Toronto</li><li>Melbourne</li><li>Hamburgo</li></ul>',
+    '<li>Todo gira alrededor de un director departamental</li>',
+    '<li>No me dió tiempo a verlo en Nuevo York</li>',
+    '<li>Esta historia no se ha llevado a la gran pantalla</li>',
+    '<li>Tiene relación con la alfarería</li>',
+  ];
+  cluesUL.innerHTML = cluesString.slice(0, daysLeft + 1).toString().replace(/,/g, '');
 }
 
 function timer() {
@@ -49,18 +44,23 @@ function timer() {
 
 function download() {
   let { value } = document.querySelector('#magicword');
-  const options = ['Me temo que no', 'Venga esfuerzate un poco mas', 'Creo que no estas entendiendo el juego'];
+  const options = ['Me temo que no', 'Venga esfuerzate un poco mas', 'Creo que no', 'Te estas equivocando', 'Venga va piensa', 'Uyy casi', 'Frio frio', 'Caliente caliente'];
 
   if (value) {
     value = value.trim().toLowerCase().replace(/\s/g, '');
     if (/harry|potter|musical|cursed|child/.test(value)) {
       const element = document.createElement('a');
-      element.setAttribute('href', 'http://localhost:8080/img/collage.png');
+      element.setAttribute('href', './saludo.zip');
       element.setAttribute('download', 'saludo.zip');
       element.style.display = 'none';
       document.body.appendChild(element);
       element.click();
       document.body.removeChild(element);
+      clearInterval(countdown);
+      daysElement.textContent = '00:';
+      hoursElement.textContent = '00:';
+      minutesElement.textContent = '00:';
+      secondsElement.textContent = '00';
     } else {
       document.querySelector('#fail').style.display = 'flex';
       document.querySelector('#fail > span').innerHTML = options[Math.floor(Math.random() * options.length)];
